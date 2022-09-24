9b26f1919fddacff3918513f09b3285f2e5acb31
Kate Raf <KateSu1989@gmail.com>
2022-09-24 16:29:17 +0500

    Example 011 Array 3

---
+++ Example011_Array3/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection)  //метод заполнения массива
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)  //метод печати массива на экран
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)  //метод нахождения индекса числа из массива
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  //если элемента нет в массиве, то будет выводить -1, а не позицию
+
+    while (index < count)
+    {
+       if(collection[index] == find)
+       {
+        position = index;
+        break;
+       }
+        index++;
+    }
+    return position;
+
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+//array[4] = 4; дорисовали для показателя работы оператора break
+//array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 5);
+Console.WriteLine(pos);
