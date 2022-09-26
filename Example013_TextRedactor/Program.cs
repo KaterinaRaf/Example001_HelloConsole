d5d06ba1311f79b13911a0d051ba081ea9c2bd48
Kate Raf <KateSu1989@gmail.com>
2022-09-26 21:31:22 +0500

    Example 013 Text Redactor

--- Example013_TextRedactor/Program.cs
+++ Example013_TextRedactor/Program.cs
@@ -15,9 +15,21 @@ string text = "- Я думаю, - сказал князь, улыбаясь, - �
 
 string Replace(string text, char oldValue, char newValue)
 {
-    string result = String.Empty;  // видео на 23 минуте
+    string result = String.Empty;  
     
     int length = text.Length;
-    
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
     return result;
 }
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
