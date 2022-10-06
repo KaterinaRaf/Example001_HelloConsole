55d627c0f6c6c38855b28c5d42867c292afc370e
Kate Raf <KateSu1989@gmail.com>
2022-10-06 08:33:28 +0500

    Example 017 Recursia

---
+++ Example017_Recursia/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пример рекурсии на факториале. 
+// Рекурсия = функция вызывает саму себя внутри тела
+
+/*double Factorial (int n) // int не справлется с большим кол-вом данных
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)    // при больших значениях происходит переполнение типа данных
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+} */
+
+
+// числа Фибонначи
+// f(1) = 1
+// f(2) = 1
+// f(n) f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}"); 
+} 
